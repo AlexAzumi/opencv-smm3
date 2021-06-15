504ad72cf8a65b5660e46233d01288ce2ac5f125
Alejandro Suárez <alejandro-hdez115@outlook.com>
2021-06-14 21:12:42 -0500

    Update `object tracking™`

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def connectToDatabase():
   connection = None
   try:
     # Create connection
-    print('Connecting to PostgreSQL database...')
+    print('PostgreSQL: Connecting to database...')
     connection = psycopg2.connect(
         host=os.getenv('DATABASE_HOST'),
         database=os.getenv('DATABASE_NAME'),
@@ -24,60 +24,57 @@ def connectToDatabase():
         password=os.getenv('DATABASE_PASSWORD'),
         port=os.getenv('DATABASE_PORT'))
 
-    # Create a cursor
-    cursor = connection.cursor()
-
-    # Execute a statement
-    print('PostgreSQL database version:')
-    cursor.execute('SELECT version()')
-
-    # Display the PostgreSQL database server version
-    db_version = cursor.fetchone()
-    print(db_version)
+    print("PostgreSQL: CONNECTED!")
 
     return connection
   except:
-    print('Cannot connect to database')
+    print('PostgreSQL: An error ocurred while connecting to the database')
     return None
 
 
 def saveInDatabase(connection):
   try:
     connection.cursor().execute(
-        'INSERT INTO "public"."vehicles" (time, cv_id) VALUES (CURRENT_TIMESTAMP)')
+        'INSERT INTO "public"."vehicles" (time) VALUES (CURRENT_TIMESTAMP)')
     # Save changes
     connection.commit()
   except Exception as e:
-    print('Error while saving in database')
+    print('PostgreSQL: Error while saving in database')
     print(e)
 
 
 def main():
+  videoParam = ''
+  videoIndex = 0
   savedVehicles = []
   # Connect to the PostgreSQL database
   dbConnection = connectToDatabase()
 
   try:
-    videoParam = str(sys.argv[1])
+    videoParam = str(sys.argv[1]) or '-video'
+    videoIndex = int(sys.argv[2]) or 0
   except:
-    videoParam = 'video_1'
+    videoParam = '-video'
 
   capture = None
   region = None
 
   # Apply the corresponding video
-  if videoParam == 'video_1':
-    capture = cv2.VideoCapture("highway_1.mp4")
+  if videoParam == '-video' and videoIndex == 0:
+    capture = cv2.VideoCapture("example_1.mp4")
+    region = [580, 720, 580, 1280]
+  elif videoParam == '-video' and videoIndex == 1:
+    capture = cv2.VideoCapture("example_2.mp4")
     region = [340, 720, 500, 800]
-  elif videoParam == 'video_2':
-    capture = cv2.VideoCapture("highway_2.m4v")
-    region = [600, 720, 600, 1280]
-  elif videoParam == 'video_3':
-    capture = cv2.VideoCapture("highway_3.mp4")
+  elif videoParam == '-video' and videoIndex == 2:
+    capture = cv2.VideoCapture("example_3.mp4")
     region = [150, 360, 0, 500]
+  else:
+    print("App: Video not found. Exiting program...")
+    exit(0)
 
-  # Trained XML classifier describes some features of some objects we want to detect
-  car_cascade = cv2.CascadeClassifier("carx.xml")
+  # Trained XML classifier that describes some features of some objects we want to detect
+  car_cascade = cv2.CascadeClassifier("cars.xml")
 
   while True:
     ret, frame = capture.read()
@@ -89,8 +86,17 @@ def main():
 
     cars = car_cascade.detectMultiScale(gray, 1.1, 2)
 
-    for (x, y, w, h) in cars:
+    # Object tracking
+    tracked_objects = tracker.update(cars)
+
+    for (x, y, w, h, id) in tracked_objects:
       cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 255), 3)
+      cv2.putText(roi, str(id), (x, y - 15),
+                  cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+      # Save in database
+      if id not in savedVehicles:
+        saveInDatabase(dbConnection)
+        savedVehicles.append(id)
 
     #cv2.imshow("Region of interest", roi)
     cv2.imshow("Computer Vision | Luxtlalli", frame)
